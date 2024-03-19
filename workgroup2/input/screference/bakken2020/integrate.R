c39e9bf8f307d595227be317395b4a484a3817cb
Martijn Vochteloo <57527605+mvochteloo@users.noreply.github.com>
2024-03-19 11:45:37 +0100

    clean up workgroup2 code part 2

--- workgroup2/input/screference/bakken2020/integrate.R
+++ workgroup2/input/screference/bakken2020/integrate.R
@@ -8,6 +8,7 @@
 # Function: integrate the human motor cortex reference files.
 ############################################################################################################################
 
+.libPaths("/usr/local/lib/R/site-library")
 library(Seurat)
 library(feather)
 args <- commandArgs(trailingOnly = TRUE)
@@ -34,41 +35,5 @@ mo.int <- IntegrateData(
   dims = 1:30
 )
 mo.int <- RunPCA(mo.int, verbose = FALSE)
-
-get_minor_to_major_subclass_dict <- function(){
-  cell_type_dict <- list()
-  # major cell types
-  cell_type_dict[["Astro"]] <- "AST"
-  cell_type_dict[["Endo"]] <- "END"
-  cell_type_dict[["L2/3 IT"]] <- "EX"
-  cell_type_dict[["L5 ET"]] <- "EX"
-  cell_type_dict[["L5 IT"]] <- "EX"
-  cell_type_dict[["L5/6 NP"]] <- "EX"
-  cell_type_dict[["L6 CT"]] <- "EX"
-  cell_type_dict[["L6 IT"]] <- "EX"
-  cell_type_dict[["L6 IT Car3"]] <- "EX"
-  cell_type_dict[["L6b"]] <- "EX"
-  cell_type_dict[["Lamp5"]] <- "IN"
-  cell_type_dict[["Micro-PVM"]] <- "MIC"
-  cell_type_dict[["Oligo"]] <- "OLI"
-  cell_type_dict[["OPC"]] <- "OPC"
-  cell_type_dict[["Pvalb"]] <- "IN"
-  cell_type_dict[["Sncg"]] <- "IN"
-  cell_type_dict[["Sst"]] <- "IN"
-  cell_type_dict[["Sst Chodl"]] <- "IN"
-  cell_type_dict[["Vip"]] <- "IN"
-  cell_type_dict[["VLMC"]] <- "PER"
-  return(cell_type_dict)
-}
-cell_type_dict <- get_minor_to_major_subclass_dict()
-
-Idents(object = mo.int) <- "subclass_label"
-mo.int$class <- as.factor(mo.int$class_label)
-mo.int$cluster <- as.factor(mo.int$cluster_label)
-major_subclass <- cell_type_dict[mo.int$subclass_label]
-names(major_subclass) <- names(mo.int$subclass_label)
-mo.int$major_subclass <- as.factor(unlist(major_subclass))
-mo.int$minor_subclass <- as.factor(mo.int$subclass_label)
-mo.int$cross_species_cluster <- as.factor(mo.int$cross_species_cluster_label)
 
 saveRDS(object = mo.int, file = args[3])
