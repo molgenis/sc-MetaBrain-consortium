bccd98caff31661658446da2b5f0c3c730d4b6d2
Martijn Vochteloo <57527605+mvochteloo@users.noreply.github.com>
2024-09-05 19:50:42 +0200

    mbQTL: fix typo + convert alpha to zscore instead of each pvalue for speedup

--- mbQTL/scripts/results.py
+++ mbQTL/scripts/results.py
@@ -20,6 +20,10 @@ for arg in vars(args):
     print("  --{} {}".format(arg, getattr(args, arg)))
 print("")
 
+if args.alpha < 0 or args.alpha > 1:
+    print("Error in --alpha outside range.")
+    exit()
+
 dir = os.path.dirname(args.outfile)
 if dir != "":
     os.makedirs(dir, exist_ok=True)
@@ -29,6 +33,19 @@ def gzopen(file, mode="r"):
         return gzip.open(file, mode + 't')
     else:
         return open(file, mode)
+
+def pvalue_to_zscore(pvalue):
+    if pvalue >= 0.9999999999999999:
+        return -1.3914582123358836e-16
+    elif pvalue <= 1e-323:
+        return -38.467405617144344
+
+    return stats.norm.ppf(pvalue / 2)
+
+######################################################
+
+# Convert the alpha to a z-score for speed-up later.
+alpha_zscore = abs(pvalue_to_zscore(pvalue=args.alpha))
 
 # Define the columns we want to keep track of and how we want to summarise them.
 columns = [
@@ -58,8 +75,6 @@ for i, fpath in enumerate(glob.glob(os.path.join(args.indir, "*/*-TopEffectsWith
     # Parse the file path.
     cov = fpath.split(os.sep)[-2]
     prefix = fpath.split(os.sep)[-1].replace("-TopEffectsWithMultTest.txt", "")
-    if prefix != "Excitatory.neurons.BryoisEffects.QTLMeta.FISHERZMETA":
-        continue
 
     # Prepare the counters.
     row = {"Cov": cov, "Prefix": prefix, "NrTestedGenes": 0}
@@ -70,29 +85,44 @@ for i, fpath in enumerate(glob.glob(os.path.join(args.indir, "*/*-TopEffectsWith
     with gzopen(fpath, 'r') as f:
         for j, line in enumerate(f):
             values = line.rstrip("\n").split("\t")
+
+            # Process the header.
             if j == 0:
-                # Process the header.
                 found = False
                 for index, value in enumerate(values):
+                    # Check if it is a column with brackets in it (e.g. per dataset column)
                     match = re.match("[A-Za-z]+\(([^()]+)\)[-A-Za-z]*", value)
                     if match:
+                        # Replace the dataset string with a placeholder to standardize the
+                        # column name.
                         colname = value.replace(match.group(1), "DATASET")
+
+                        # Save which dataset is stored at which index in the dataset
+                        # string.
                         ds_indices = {}
                         for ds_index, dataset in enumerate(match.group(1).split(";")):
                             ds_indices[ds_index] = dataset
+
+                            # Replace the placeholder with the actual dataset and add this
+                            # colname to the row and colnames_order if we wish to keep
+                            # it.
                             ds_colname = colname.replace("DATASET", dataset)
                             if colname in columns_order:
                                 row[ds_colname] = 0
                                 if ds_colname not in colnames_order:
                                     colnames_order[ds_colname] = columns_order[colname]
+
                         sub_indices[colname] = ds_indices
                     else:
+                        # Else, simply store the colname if we wish to save it.
                         colname = value
                         if colname in columns_order:
                             row[colname] = 0
                             if colname not in colnames_order:
                                 colnames_order[colname] = columns_order[colname]
 
+                    # Store the location of the standardized column name to use
+                    # as a selection dict for the next lines.
                     indices[colname] = index
                 continue
 
@@ -102,29 +132,37 @@ for i, fpath in enumerate(glob.glob(os.path.join(args.indir, "*/*-TopEffectsWith
             # Process the columns of interest based on their mode.
             for colname, mode in columns:
                 if colname in indices:
+                    # Extract the value of the current column usin the index dict
+                    # we created from the header.
                     value = values[indices[colname]]
+
+                    # Process the different modes.
                     if mode == "snp":
+                        # Count SNPs (e.g. alleles string has length 3)
                         if len(value) == 3:
                             row[colname] += 1
                     elif mode == "sum" or mode == "average":
+                        # Keep a total count.
                         row[colname] += int(value)
                     elif mode == "signif":
+                        # Check if a value is below a certain threshold.
                         if float(value) < args.alpha:
                             row[colname] += 1
                     elif mode == "dataset_z_signif":
+                        # Per dataset, convert the z-score to p-values and
+                        # check if it is below a certain threshold.
                         for ds_index, ds_zscore in enumerate(value.split(";")):
                             if ds_zscore == "-":
                                 continue
                             ds_colname = colname.replace("DATASET", sub_indices[colname][ds_index])
-                            ds_pvalue = stats.norm.cdf(-abs(float(ds_zscore))) * 2
-                            if ds_pvalue < args.alpha:
+                            if abs(float(ds_zscore)) >= alpha_zscore:
                                 row[ds_colname] += 1
                     else:
                         print("Error, unexpected mode '{}'.".format(mode))
                         exit()
     f.close()
 
-    # Post-process the averge columns.
+    # Post-process the average columns.
     for colname, mode in columns:
         if mode == "average":
             row[colname] = round(row[colname] / row["NrTestedGenes"], 1)
@@ -140,7 +178,9 @@ if len(data) == 0:
 # Convert to Pandas.
 df = pd.DataFrame(data).T
 
-# Sort.
+# Sort. In the case of fisherzmetarandom the MetaP is replaced with MetaP-Random and
+# as such we cannot assume that MetaP exists. To combat this I will take the lowest value
+# of either column, sort on that, and then remove the tmp column.
 pvalue_sort_col = "MetaP"
 if "MetaP-Random" in df:
     df["tmpP"] = df[["MetaP", "MetaP-Random"]].min(axis=1)
@@ -153,7 +193,7 @@ if "tmpP" in df.columns:
 colnames_order = list(colnames_order.items())
 colnames_order.sort(key=lambda x: (x[1], x[0]))
 order = [co[0] for co in colnames_order]
-df = df.loc[:, ["Cov", "Prefix", "NrTestedGenes"] + order].rename(columns=rename_columns).T
+df = df.loc[:, ["Cov", "Prefix", "NrTestedGenes"] + order].rename(columns=rename_columns)
 
 print("\nResults:")
 print(df)
