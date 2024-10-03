4c7c53bc50fc7c7b798e2e531fe125565681b20e
anoekkooijmans <kooijmans.a@outlook.com>
2024-10-03 13:22:41 +0200

    Updated normalisation

--- workgroup4/co_expr_qtl/scripts/normalise.py
+++ workgroup4/co_expr_qtl/scripts/normalise.py
@@ -7,23 +7,24 @@ import json
 import numpy as np
 import scipy as sp
 import os
+import scanpy
+from scipy.io import mmwrite
+from scipy.io import mmread
+import h5py
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--counts", required=True, type=str, help="")
 parser.add_argument("--avg_rd", required=True, type=str, help="")
 parser.add_argument("--log1p", action="store_true", default=False, help="")
+parser.add_argument("--weights_out", required=False, type=str, default=None, help="Path to the output weights file.")
 parser.add_argument("--out", required=True, type=str, help="")
+
 args = parser.parse_args()
 
 print("Options in effect:")
 for arg in vars(args):
     print("  --{} {}".format(arg, getattr(args, arg)))
 print("")
-
-dir = os.path.dirname(args.outfile)
-if dir != "":
-    os.makedirs(dir, exist_ok=True)
-
 
 def gzopen(file, mode="r"):
     if file.endswith(".gz"):
@@ -36,12 +37,44 @@ def do_pf(mtx, sf):
     pf = sp.sparse.diags(sf/pf) @ mtx
     return pf
 
+def save_filtered_counts_h5(fpath, adata):
+    """
+    Write hdf5 file from Cell Ranger v4 or later versions.
+    Source: https://github.com/scverse/anndata/issues/595
+    """
+    if os.path.exists(fpath):
+        raise FileExistsError(f"There already is a file `{fpath}`.")
+
+    int_max = lambda x: int(max(np.floor(len(str(int(max(x)))) / 4), 1) * 4)
+    str_max = lambda x: max([len(i) for i in x])
+
+    # Save.
+    w = h5py.File(fpath, 'w')
+    grp = w.create_group("matrix")
+    grp.create_dataset("barcodes", data=np.array(adata.obs_names, dtype=f'|S{str_max(adata.obs_names)}'))
+    grp.create_dataset("data", data=np.array(adata.X.data, dtype=f'<i{int_max(adata.X.data)}'))
+    ftrs = grp.create_group("features")
+    # this group will lack the following keys:
+    # '_all_tag_keys', 'feature_type', 'genome', 'id', 'name', 'pattern', 'read', 'sequence'
+    ftrs.create_dataset("feature_type", data=np.array(adata.var.feature_types, dtype=f'|S{str_max(adata.var.feature_types)}'))
+    ftrs.create_dataset("genome", data=np.array(adata.var.genome, dtype=f'|S{str_max(adata.var.genome)}'))
+    ftrs.create_dataset("id", data=np.array(adata.var.gene_ids, dtype=f'|S{str_max(adata.var.gene_ids)}'))
+    ftrs.create_dataset("name", data=np.array(adata.var.index, dtype=f'|S{str_max(adata.var.index)}'))
+    grp.create_dataset("indices", data=np.array(adata.X.indices, dtype=f'<i{int_max(adata.X.indices)}'))
+    grp.create_dataset("indptr", data=np.array(adata.X.indptr, dtype=f'<i{int_max(adata.X.indptr)}'))
+    grp.create_dataset("shape", data=np.array(list(adata.X.shape)[::-1], dtype=f'<i{int_max(adata.X.shape)}'))
+
+
 # def norm_pf_log_pf(mtx):
 #     pf_log_pf = do_pf(np.log1p(do_pf(mtx)))
 #     return pf_log_pf
 
 print("Loading count ...")
-mtx = None
+adata = scanpy.read_10x_h5(args.counts)
+mtx = adata.X
+barcodes = adata.obs_names
+
+print(f"Original matrix shape: {mtx.shape}")
 
 print("Loading average read count ...")
 fh = open(args.avg_rd)
@@ -56,6 +89,15 @@ if args.log1p:
     print("log1p transform ...")
     mtx = np.log1p(mtx)
 
+    print("Calculating read counts ...")
+    pf = mtx.A.sum(axis=1).ravel().astype(int)
+    print(f"Matrix shape after log {mtx.shape}")
+    with gzip.open(args.weights_out, "wt") as fh:
+        for idx in range(0,len(barcodes)):
+            fh.write(f"{barcodes[idx]}\t{pf[idx]}\n")
+
 print("Saving count ...")
+adata.X = mtx
+save_filtered_counts_h5(args.out, adata)
 
 print("Done.")
