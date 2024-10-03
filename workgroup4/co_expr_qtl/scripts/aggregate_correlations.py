64c5e8063ea0aed7e570e5ff997a4bcec270f1bd
anoekkooijmans <kooijmans.a@outlook.com>
2024-10-03 13:31:43 +0200

    Updated aggregate_correlations

--- workgroup4/co_expr_qtl/scripts/aggregate_correlations.py
+++ workgroup4/co_expr_qtl/scripts/aggregate_correlations.py
@@ -4,11 +4,13 @@
 import argparse
 import gzip
 import os
+import numpy as np
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("--indir", required=True, type=str,  help="")
-parser.add_argument("--chr", required=False, type=str, help="")
-parser.add_argument("--out", required=True, type=str, help="")
+parser.add_argument("--input", required=True,  nargs="*", help="Individual correlation files per sample")
+parser.add_argument("--gene_pairs", required=True, type=str, help="List of gene pairs for a chromosome")
+parser.add_argument("--out", required=True, type=str, help="Output file")
 args = parser.parse_args()
 
 print("Options in effect:")
@@ -16,16 +18,58 @@ for arg in vars(args):
     print("  --{} {}".format(arg, getattr(args, arg)))
 print("")
 
-os.makedirs(os.path.dirname(args.out), exist_ok=True)
+def get_sample_id(filepath):
+    file = filepath.split('/')[-1]
+    pool_sample = file.split(".")[0:2]
+    sample_id = ".".join(pool_sample)
+    return sample_id                
 
+print(f"Reading in gene pairs...")
+with gzip.open(args.gene_pairs,"rt") as f:
+    for line in f:
+        gene_pairs = line.strip().split("\t")
+        break
+f.close()
 
-def gzopen(file, mode="r"):
-    if file.endswith(".gz"):
-        return gzip.open(file, mode + 't')
-    else:
-        return open(file, mode)
+n_gene_pairs = len(gene_pairs)
 
+print("Creating gene indexing dictionary...")
+gene_pair_idx = {gene_pair: i for i, gene_pair in enumerate(gene_pairs)}
 
-print("Loading file ...")
+print(f"Writing aggregated file...")
+fileOut = gzip.open(args.out,"at")
+fileOut.write("\t")
+fileOut.write("\t".join(gene_pairs))
+fileOut.write("\n")
 
+corr_array = np.full(n_gene_pairs, np.nan)
+header = None
+counter = 0
+for file in args.input:
+    sample_id = get_sample_id(file)
+    print(f"Processing {sample_id}...")
+    with gzip.open(file,"rt") as f:
+        for line in f:
+            counter += 1
+            if header == None:
+                header = line
+                continue
+            values = line.strip().split("\t")
+            gene1 = values[0]
+            gene2 = values[1]
+            corr = values[2]
+
+            if f"{gene1}_{gene2}" in gene_pair_idx:
+                row_idx = gene_pair_idx[f"{gene1}_{gene2}"]
+                corr_array[row_idx] = corr
+                continue
+            elif f"{gene2}_{gene1}" in gene_pair_idx:
+                row_idx = gene_pair_idx[f"{gene2}_{gene1}"]
+                corr_array[row_idx] = corr
+                continue
+
+    fileOut.write(f"{sample_id}\t")
+    fileOut.write("\t".join(map(str, corr_array))) 
+    fileOut.write("\n")
+fileOut.close()
 print("Done.")
