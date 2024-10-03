6d3ad756994159f0e4f97fdaf26de27a45493209
anoekkooijmans <kooijmans.a@outlook.com>
2024-10-03 13:33:34 +0200

    Updated create_annotation

--- workgroup4/co_expr_qtl/scripts/create_annotation_file.py
+++ workgroup4/co_expr_qtl/scripts/create_annotation_file.py
@@ -2,14 +2,20 @@
 # Author: M. Vochteloo
 
 import argparse
+import pandas as pd
+import numpy as np
 import gzip
 import os
+import cProfile
+import pstats
+
+profiler = cProfile.Profile()
+profiler.enable()
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--in_gtf", required=True, type=str, help="")
 parser.add_argument("--gene_pairs", required=True, type=str, help="")
-parser.add_argument("--feature_name", required=False, type=str, default="gene_name", help="")
-parser.add_argument("--autosomes_only", action="store_true", default=False, help="")
+parser.add_argument("--chr", required=False, type=str, default="gene_name", help="")
 parser.add_argument("--out", required=True, type=str, help="")
 args = parser.parse_args()
 
@@ -20,63 +26,53 @@ print("")
 
 os.makedirs(os.path.dirname(args.out), exist_ok=True)
 
+# Read in correlation gene pairs
+print(f"Reading feature pairs corresponding to chromosome {args.chr} ...")
+with gzip.open(args.gene_pairs,"rt") as f:
+    for line in f:
+        feature_pairs = line.strip().split("\t")
 
-def gzopen(file, mode="r"):
-    if file.endswith(".gz"):
-        return gzip.open(file, mode + 't')
-    else:
-        return open(file, mode)
+print(f"Processing {len(feature_pairs)} feature pairs ...")
+feature_dict = {}
+for x in feature_pairs:
+    feature = x.split("_")[0]
+    feature_dict[feature] = np.empty(4,dtype="U50")
 
-
-print("Parsing annotation file...")
-
-tmp_fpath = args.out + ".WithDuplicates.txt.gz"
-fhtmp = gzopen(tmp_fpath, mode="w")
-fhtmp.write("\t".join(["Platform", "ArrayAddress", "Symbol", "Chr", "ChrStart", "ChrEnd", "Probe", "Strand"]) + "\n")
-
-accepted_chr = [str(i) for i in range(1, 23)]
-if not args.autosomes_only:
-    accepted_chr.extend(["X", "MT", "M", "Y"])
-
-features = set()
-duplicated = set()
-with gzopen(args.in_gtf, mode="r") as f:
+# Extract info from gtf file 
+print("Reading gtf annotation file ...")
+id_dict = {}
+with open(args.in_gtf, mode="r") as f:
     for line in f:
         if line.startswith("#"):
             continue
-
         seqname, source, feature, start, end, score, strand, frame, attribute = line.rstrip("\n").split("\t")
         if feature != "gene":
             continue
         attributes = {key: val.replace('"', '') for key, val in (attr.split(' ') for attr in attribute.split("; "))}
+        if attributes["gene_name"] in feature_dict.keys():
+            feature_dict[attributes["gene_name"]][0] = source
+            feature_dict[attributes["gene_name"]][1] = start
+            feature_dict[attributes["gene_name"]][2] = end
+            feature_dict[attributes["gene_name"]][3] = strand
+        id_dict[ attributes["gene_name"]] = attributes["gene_id"]
 
-        if attributes[args.feature_name] in features:
-            duplicated.add(attributes[args.feature_name])
-
-        if seqname.startswith("chr"):
-            seqname = seqname.replace("chr", "")
-
-        if seqname not in accepted_chr:
-            continue
-
-        fhtmp.write("\t".join([source, attributes[args.feature_name], attributes["gene_name"], seqname, start, end, attributes["gene_id"], strand]) + "\n")
-        features.add(attributes[args.feature_name])
-f.close()
-fhtmp.close()
-
-print("  Saved {:,} features with {:,} duplicates".format(len(features), len(duplicated)))
-
-fhout = gzopen(args.out + ".txt.gz", mode="w")
-n_removed = 0
-with gzopen(tmp_fpath, mode="r") as f:
-    for line in f:
-        if line.rstrip("\n").split("\t")[1] in duplicated:
-            n_removed += 1
-            continue
-        fhout.write(line)
-f.close()
-fhout.close()
-
-print("  Removed {:,} features with multiple entries".format(n_removed))
+# Write annotation for each gene pair
+print(f"Writing annotation file ...")
+fileOut = gzip.open(f"{args.out}chr.{args.chr}.annotation.txt.gz","wt")
+fileOut.write("Platform\tArrayAddress\tSymbol\tChr\tChrStart\tChrEnd\tProbe\tStrand\n")
+for pair in feature_pairs:
+    array_adress = pair
+    ft0,ft1 = x.split("_")
+    platform = feature_dict[ft0][0]
+    chr_start = feature_dict[ft0][1]
+    chr_end = feature_dict[ft0][2]
+    strand = feature_dict[ft0][3]
+    symbol = f"{id_dict[ft0]}_{id_dict[ft1]}" # Get ensg symbol
+    fileOut.write(f"{platform}\t{array_adress}\t{symbol}\t{args.chr}\t{chr_start}\t{chr_end}\t{symbol}\t{strand}\n")
+fileOut.close()
 
 print("Done.")
+
+profiler.disable()
+stats = pstats.Stats(profiler).sort_stats('cumtime')
+stats.print_stats(40)
