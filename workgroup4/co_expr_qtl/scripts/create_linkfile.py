39dcaafe3524f0a635b19a5ea8a1449aa0816cca
anoekkooijmans <kooijmans.a@outlook.com>
2024-11-21 13:05:28 +0100

    Updated create_linkfile

--- workgroup4/co_expr_qtl/scripts/create_linkfile.py
+++ workgroup4/co_expr_qtl/scripts/create_linkfile.py
@@ -1,12 +1,10 @@
 import gzip
 import pandas as pd
 import argparse
-import sys
-import os
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("--sample_ids", required=True, type=str,  help="")
-parser.add_argument("--wg0_file_directories", required=True, type=str,  help="")
+parser.add_argument("--corr", required=True, type=str,  help="")
+parser.add_argument("--wg0", required=True, type=str,  help="")
 parser.add_argument("--output", required=True, type=str,  help="")
 
 args = parser.parse_args()
@@ -16,33 +14,40 @@ for arg in vars(args):
     print("  --{} {}".format(arg, getattr(args, arg)))
 print("")
 
+def gzopen(file, mode="r"):
+    if file.endswith(".gz"):
+        if mode == "w":
+            return gzip.open(file, mode + 't', 4)
+        else:
+            return gzip.open(file, mode + 't')
+    else:
+        return open(file, mode)
 
-fh = open(args.sample_ids,"rt")
-elems = []
-for line in fh:
-    elems.append(line.strip())
-fh.close()
 
-print(f"{len(elems)} sample ids")
+print("Exctracting sample IDs from correlation file header")
+fin = gzopen(args.corr,"r")
+samples = fin.readline().strip().split("\t")[1:]
+fin.close()
 
-fho = open(args.output,'w')
-# header = fh.readline()
-seen = set()
+print(f"{len(samples)} sample IDs found")
 
-df = pd.read_csv(args.wg0_file_directories,sep="\t")
+print("\nLinking sample IDs to dataset use WG0 poolsheet")
+df = pd.read_csv(args.wg0,sep="\t")
 pool_dataset = df.groupby('Pool')['Dataset'].apply(list).to_dict()
+print(f"{len(pool_dataset)} pool-dataset links found")
 
-print(f"{len(pool_dataset)} pool-dataset links")
-
+print("\nWriting linkfile")
+fho = gzopen(args.output,'w')
+seen = set()
 wctr = 0
-for elem in elems:
-    pool,ind = elem.split(".")
-    if ind not in seen:
-        seen.add(ind)
+for sample in samples:
+    pool,id = sample.split(".")
+    if id not in seen:
+        seen.add(id)
         dataset = pool_dataset[pool][0]
-        fho.write(ind+"\t"+elem+"\t"+dataset+"\n")
+        fho.write(id+"\t"+sample+"\t"+dataset+"\n")
         wctr += 1
 fho.close()
 
 print(f"{wctr} unique samples written")
-print("Done.")
+print("\nDone.\n")
