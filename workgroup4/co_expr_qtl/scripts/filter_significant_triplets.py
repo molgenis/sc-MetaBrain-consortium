9770aebae973c6971c5ef9ffe3c085947ccb2b4e
anoekkooijmans <kooijmans.a@outlook.com>
2024-11-21 14:42:17 +0100

    Updated co-eqtl dump

--- workgroup4/co_expr_qtl/scripts/filter_significant_triplets.py
+++ workgroup4/co_expr_qtl/scripts/filter_significant_triplets.py
@@ -3,7 +3,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--significant_egenes", required=True, nargs="*", type=str,  help="")
-parser.add_argument("--input", required=True, type=str,  help="")
+parser.add_argument("--egene_column", required=True, type=str,  help="")
+parser.add_argument("--signif_column", required=True, type=str,  help="")
+parser.add_argument("--indir", required=True, type=str,  help="")
 parser.add_argument("--output", required=True, type=str,  help="")
 
 args = parser.parse_args()
@@ -13,21 +15,23 @@ for arg in vars(args):
     print("  --{} {}".format(arg, getattr(args, arg)))
 print("")
 
+print(f"Extracting significance using {args.signif_column}")
 fin = open(args.significant_egenes,"rt")
+header = fin.readline().strip().split("\t")
 fin.readline()
 egene_pval = {}
 for line in fin:
-	egene = line.strip().split("\t")[0]
-	pval = line.strip().split("\t")[-1]
+	egene = line.strip().split("\t")[header.index(args.egene_col)]
+	pval = line.strip().split("\t")[header.index(args.signif_column)]
 	egene_pval[egene] = pval
 fin.close()
 
-files = args.input
-out = args.output
-
+print(f"Looking for files in: {args.indir}")
+pattern = f"{args.indir}*-dump-TopEffects.txt"
+files = glob.glob(pattern)
 print(f"{len(files)} files detected")
 
-fho = open(out,'w')
+fho = open(args.out,'w')
 print("open outputfile")
 ctr = 0
 wctr = 0
